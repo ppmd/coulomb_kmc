1d22d52cadef44759076e7cd1c3a6a8818ac4401
William <wrs20@bath.ac.uk>
2019-03-13 13:27:53 +0000

    free space and nearest 27 direct C calculators

--- tests/host/kmc_test_common.py
+++ tests/host/kmc_test_common.py
@@ -7,6 +7,7 @@ from ppmd.lib import build
 REAL = ctypes.c_double
 INT64 = ctypes.c_int64
 
+from itertools import product
 
 class FreeSpaceDirect:
     def __init__(self):
@@ -54,7 +55,7 @@ class FreeSpaceDirect:
                     const REAL r2 = d0*d0 + d1*d1 + d2*d2;
                     const REAL r = sqrt(r2);
 
-                    tmp_inner_phi += Q[ix] * Q[jx] / r;
+                    tmp_inner_phi += iq * jq / r;
 
                 }}
                 
@@ -69,6 +70,121 @@ class FreeSpaceDirect:
 
 
         self._lib = build.simple_lib_creator(header_code=header, src_code=src, name="kmc_fmm_free_space_direct")['free_space_direct']
+    
+
+    def __call__(self, N, P, Q):
+
+        phi = ctypes.c_double(0)
+
+        self._lib(
+            INT64(N),
+            P.ctypes.get_as_parameter(),
+            Q.ctypes.get_as_parameter(),
+            ctypes.byref(phi)
+        )
+        
+        return phi.value
+
+
+class NearestDirect:
+    def __init__(self, E):
+
+        ox_range = tuple(range(-1, 2))
+
+        inner = ''
+
+        for oxi, ox in enumerate(product(ox_range, ox_range, ox_range)):
+                if ox[0] != 0 or ox[1] != 0 or ox[2] != 0:
+                    inner += """
+                            d0 = jp0 - ip0 + {OX};
+                            d1 = jp1 - ip1 + {OY};
+                            d2 = jp2 - ip2 + {OZ};
+                            r2 = d0*d0 + d1*d1 + d2*d2;
+                            r = sqrt(r2);
+                            tmp_inner_phi += 0.5 * iq * jq / r;
+
+                    """.format(
+                        OXI=oxi,
+                        OX=ox[0] * E,
+                        OY=ox[1] * E,
+                        OZ=ox[2] * E
+                    )
+        
+        
+        header = r"""
+        #include <math.h>
+        #define INT64 int64_t
+        #define REAL double
+        """
+
+        src = r"""
+        
+        extern "C" int nearest_direct(
+            const INT64 N,
+            const REAL * RESTRICT P,
+            const REAL * RESTRICT Q,
+            REAL * RESTRICT phi
+        ){{
+
+            REAL tmp_phi = 0.0;
+
+            #pragma omp parallel for reduction(+:tmp_phi)
+            for(INT64 ix=0 ; ix<N ; ix++){{
+                REAL tmp_inner_phi = 0.0;
+                
+                const REAL iq = Q[ix];
+                const REAL ip0 = P[3*ix + 0];
+                const REAL ip1 = P[3*ix + 1];
+                const REAL ip2 = P[3*ix + 2];
+
+                for(INT64 jx=(ix+1) ; jx<N ; jx++){{
+                    
+                    const REAL jq = Q[jx];
+                    const REAL jp0 = P[3*jx + 0];
+                    const REAL jp1 = P[3*jx + 1];
+                    const REAL jp2 = P[3*jx + 2];
+
+                    REAL d0 = ip0 - jp0;
+                    REAL d1 = ip1 - jp1;
+                    REAL d2 = ip2 - jp2;
+                    
+                    REAL r2 = d0*d0 + d1*d1 + d2*d2;
+                    REAL r = sqrt(r2);
+
+                    tmp_inner_phi += iq * jq / r;
+
+                }}
+
+                for(INT64 jx=0 ; jx<N ; jx++){{
+                    
+                    const REAL jq = Q[jx];
+                    const REAL jp0 = P[3*jx + 0];
+                    const REAL jp1 = P[3*jx + 1];
+                    const REAL jp2 = P[3*jx + 2];
+
+                    REAL d0;
+                    REAL d1;
+                    REAL d2;
+                    
+                    REAL r2;
+                    REAL r;
+
+                    {INNER}
+
+                }}
+                
+                tmp_phi += tmp_inner_phi;
+
+            }}
+           
+            phi[0] = tmp_phi;
+            return 0;
+        }}
+        """.format(
+            INNER=inner
+        )
+
+        self._lib = build.simple_lib_creator(header_code=header, src_code=src, name="kmc_fmm_nearest_direct")['nearest_direct']
 
 
     def __call__(self, N, P, Q):
@@ -85,10 +201,19 @@ class FreeSpaceDirect:
         return phi.value
 
 
+class PBCDirect:
+    def __init__(self, E, L):
+
+        self._nd = NearestDirect(E)
 
 
+    def __call__(self, N, P, Q):
 
+        phi = ctypes.c_double(0)
 
+        sr = self._nd(N, P, Q)
+        
+        return phi.value
 
 
 
--- tests/host/test_common.py
+++ tests/host/test_common.py
@@ -48,9 +48,56 @@ def test_free_space_1():
 
         rel = abs(phi_py)
         rel = 1.0 if rel == 0 else rel
-        err = abs(phi_py - phi_c)
-        assert err < 10.**-12
+        err = abs(phi_py - phi_c) / rel
+        assert err < 10.**-14
 
-        
+
+
+ox_range = tuple(range(-1, 2))
+
+def test_nearest_1():
+
+    E = 39.
+    ND = NearestDirect(E)
+
+
+    for testx in range(max(10, 20//MPISIZE)):
+
+        rng = np.random.RandomState(seed=(MPIRANK+1)*93573)
+        N = rng.randint(1, 100)
+
+
+        ppi = np.zeros((N, 3), REAL)
+        qi = np.zeros((N, 1), REAL)
+
+        def _direct():
+            _phi_direct = 0.0
+            # compute phi from image and surrounding 26 cells
+            for ix in range(N):
+                for jx in range(ix+1, N):
+                    rij = np.linalg.norm(ppi[jx,:] - ppi[ix,:])
+                    _phi_direct += qi[ix, 0] * qi[jx, 0] / rij
+
+                for jx in range(N):
+                    for ox in product(ox_range, ox_range, ox_range):
+                        if ox[0] != 0 or ox[1] != 0 or ox[2] != 0:
+                            rij = np.linalg.norm(ppi[jx,:] - ppi[ix,:] + (E*np.array(ox)))
+                            _phi_direct += 0.5 * qi[ix, 0] * qi[jx, 0] / rij
+
+            return _phi_direct
+
+
+        ppi[:] = rng.uniform(-1.0, 1.0, (N,3))
+        qi[:] = rng.uniform(-1.0, 1.0, (N,1))
+
+        phi_py = _direct()
+        phi_c = ND(N, ppi, qi)
+
+        rel = abs(phi_py)
+        rel = 1.0 if rel == 0 else rel
+        err = abs(phi_py - phi_c) / rel
+
+        assert err < 10.**-13
+
 
 
